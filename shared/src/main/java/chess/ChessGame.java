a2da89c8b63413dfe17eb84ed5cdad3de261b8b6
Garrett Wride <garrett.wride@gmail.com>
2024-02-08 21:04:48 -0700

    implemnt more methods

--- shared/src/main/java/chess/ChessGame.java
+++ shared/src/main/java/chess/ChessGame.java
@@ -171,7 +171,7 @@ public class ChessGame {
      * @param board the new board to use
      */
     public void setBoard(ChessBoard board) {
-        throw new RuntimeException("Not implemented");
+        board.resetBoard();
     }
 
     /**
@@ -180,7 +180,7 @@ public class ChessGame {
      * @return the chessboard
      */
     public ChessBoard getBoard() {
-        throw new RuntimeException("Not implemented");
+        return this.getBoard();
     }
 
     }
