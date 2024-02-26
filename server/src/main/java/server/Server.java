319775b52e10f2664297605571d2661a0c1a4d90
Garrett Wride <garrett.wride@gmail.com>
2024-02-25 22:38:14 -0700

    registration service again

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,60 +1,41 @@
 package server;
 
 import spark.*;
-import com.google.gson.Gson;
+import service.RegistrationService;
 
 public class Server {
-    private final Gson gson = new Gson();
 
+    private RegistrationService registrationService;
 
-    public int run(int desiredPort) {
+    public Server(RegistrationService registrationService) {
+        this.registrationService = registrationService;
+    }
+
+    public void run(int desiredPort) {
         Spark.port(desiredPort);
+
+        // Define routes
+        Spark.post("/user", this::handleRegistration);
 
         Spark.staticFiles.location("web");
 
-        // Register your endpoints and handle exceptions here.
-        registerEndpoints();
-
-
-
         Spark.awaitInitialization();
-        return Spark.port();
-    }
-
-    private void registerEndpoints() {
-        // Handle POST request to /user
-        Spark.post("/user", (request, response) -> {
-            try {
-                // Extract username, password, and email from request body
-                String username = request.queryParams("username");
-                String password = request.queryParams("password");
-                String email = request.queryParams("email");
-
-                // Call register method of RegistrationService
-                String authToken = RegistrationService.register(username, password, email);
-
-                // Serialize the response object to JSON
-                String jsonResponse = gson.toJson(authToken);
-
-                // Set the content type to JSON
-                response.type("application/json");
-
-                // Return the serialized JSON response
-                return jsonResponse;
-            } catch (RegistrationException e) {
-                // Handle registration error
-                response.status(400); // Bad Request
-                return gson.toJson("Registration failed: " + e.getMessage());
-            } catch (Exception e) {
-                // Handle other unexpected errors
-                response.status(500); // Internal Server Error
-                return gson.toJson("Internal server error: " + e.getMessage());
-            }
-        });
     }
 
     public void stop() {
         Spark.stop();
         Spark.awaitStop();
+    }
+
+    private String handleRegistration(Request request, Response response) {
+        // Get the value of the "username" parameter from the request
+        String username = request.queryParams("username");
+
+// Get the value of the "password" parameter from the request
+        String password = request.queryParams("password");
+
+// Get the value of the "email" parameter from the request
+        String email = request.queryParams("email");
+        return registrationService.register(username, password, email);
     }
 }
