7c4978e07b68d5e04acc425ae099ca5adf152d4c
Garrett Wride <garrett.wride@gmail.com>
2024-02-26 09:49:40 -0700

    UserDataAccess

--- server/src/main/java/dataAccess/UserDataAccess.java
+++ server/src/main/java/dataAccess/UserDataAccess.java
@@ -1,4 +1,23 @@
 package dataAccess;
 
+import dataModels.User;
+
 public class UserDataAccess {
+    private final DataMemory dataMemory;
+
+    public UserDataAccess(DataMemory dataMemory) {
+        this.dataMemory = dataMemory;
+    }
+
+    // Method to add a new user
+    public void addUser(User user) {
+        dataMemory.addUser(user);
+    }
+
+    // Method to retrieve a user by username
+    public User getUser(String username) {
+        return dataMemory.getUser(username);
+    }
+
+    // Additional methods for updating and deleting users if needed
 }
