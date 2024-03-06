6dca5d6a05592e0083f87dd83bdd764d9c50677a
Garrett Wride <garrett.wride@gmail.com>
2024-03-06 12:06:12 -0700

    authdataaccess tests 3

--- server/src/test/java/dataAccessTests/AuthDataAccessTests.java
+++ server/src/test/java/dataAccessTests/AuthDataAccessTests.java
@@ -1,6 +1,8 @@
 package dataAccessTests;
 
 import static org.junit.Assert.*;
+
+import dataAccess.DataAccessException;
 import dataAccess.DatabaseManager;
 import dataAccess.AuthDataAccess;
 import model.AuthData;
@@ -67,5 +69,45 @@ public class AuthDataAccessTests {
         assertEquals(username, authData.getUsername());
         assertEquals(authToken, authData.getAuthToken());
     }
+
+    @Test(expected = DataAccessException.class)
+    public void testAddAuthToken_Negative_UserNotFound() throws Exception {
+        // Negative test for adding an auth token with user not found
+        // Arrange
+        String username = "nonExistentUser";
+
+        // Act
+        authDataAccess.addAuthToken(username);
+
+        // Assert
+        // Expects DataAccessException to be thrown
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testGetAuthToken_Negative_AuthTokenNotFound() throws Exception {
+        // Negative test for retrieving an auth token with non-existent auth token
+        // Arrange
+        String authToken = "nonExistentToken";
+
+        // Act
+        authDataAccess.getAuthToken(authToken);
+
+        // Assert
+        // Expects DataAccessException to be thrown
+    }
+
+    @Test(expected = DataAccessException.class)
+    public void testGetAuthData_Negative_AuthDataNotFound() throws Exception {
+        // Negative test for retrieving auth data with non-existent auth token
+        // Arrange
+        String authToken = "nonExistentToken";
+
+        // Act
+        authDataAccess.getAuthData(authToken);
+
+        // Assert
+        // Expects DataAccessException to be thrown
+    }
+
 }
 
