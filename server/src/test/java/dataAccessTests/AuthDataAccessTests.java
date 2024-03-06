e8f8895f95975971fe7b138741fabebacdbc0561
Garrett Wride <garrett.wride@gmail.com>
2024-03-06 12:02:26 -0700

    authdataaccess tests 2

--- server/src/test/java/dataAccessTests/AuthDataAccessTests.java
+++ server/src/test/java/dataAccessTests/AuthDataAccessTests.java
@@ -3,6 +3,7 @@ package dataAccessTests;
 import static org.junit.Assert.*;
 import dataAccess.DatabaseManager;
 import dataAccess.AuthDataAccess;
+import model.AuthData;
 import org.junit.*;
 import java.sql.*;
 
@@ -23,4 +24,48 @@ public class AuthDataAccessTests {
         connection.rollback();
         connection.close();
     }
+
+    @Test
+    public void testAddAuthToken_Positive() throws Exception {
+        // Positive test for adding an auth token
+        // Arrange
+        String username = "testUser";
+
+        // Act
+        String authToken = authDataAccess.addAuthToken(username);
+
+        // Assert
+        assertNotNull(authToken);
+    }
+
+    @Test
+    public void testGetAuthToken_Positive() throws Exception {
+        // Positive test for retrieving an auth token
+        // Arrange
+        String username = "testUser";
+        String authToken = authDataAccess.addAuthToken(username);
+
+        // Act
+        String retrievedAuthToken = authDataAccess.getAuthToken(authToken);
+
+        // Assert
+        assertEquals(authToken, retrievedAuthToken);
+    }
+
+    @Test
+    public void testGetAuthData_Positive() throws Exception {
+        // Positive test for retrieving auth data
+        // Arrange
+        String username = "testUser";
+        String authToken = authDataAccess.addAuthToken(username);
+
+        // Act
+        AuthData authData = authDataAccess.getAuthData(authToken);
+
+        // Assert
+        assertNotNull(authData);
+        assertEquals(username, authData.getUsername());
+        assertEquals(authToken, authData.getAuthToken());
+    }
+}
 
