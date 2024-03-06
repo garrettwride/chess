8ed63e55380a4872ce3f55fa08abc2752a7a1fb3
Garrett Wride <garrett.wride@gmail.com>
2024-03-06 11:45:07 -0700

    gamedataaccess tests 3

--- server/src/test/java/dataAccessTests/GameDataAccessTests.java
+++ server/src/test/java/dataAccessTests/GameDataAccessTests.java
@@ -8,6 +8,7 @@ import dataAccess.GameDataAccess;
 import model.GameData;
 import org.junit.*;
 import java.sql.*;
+import java.util.List;
 
 
 public class GameDataAccessTests {
@@ -91,6 +92,83 @@ public class GameDataAccessTests {
 
         // Assert
         assertNull(retrievedGame);
+    }
+
+    @Test
+    public void testGetAllGames_Positive() throws Exception {
+        // Positive test for getting all games
+        // Arrange
+        int gameID1 = 1;
+        String whiteUsername1 = "user1";
+        String blackUsername1 = "user2";
+        String gameName1 = "game1";
+        ChessGame game1 = new ChessGame(); // Create a new ChessGame object
+        GameData gameData1 = new GameData(gameID1, whiteUsername1, blackUsername1, gameName1, game1);
+        addGameToDatabase(gameData1);
+
+        int gameID2 = 2;
+        String whiteUsername2 = "user3";
+        String blackUsername2 = "user4";
+        String gameName2 = "game2";
+        ChessGame game2 = new ChessGame(); // Create another ChessGame object
+        GameData gameData2 = new GameData(gameID2, whiteUsername2, blackUsername2, gameName2, game2);
+        addGameToDatabase(gameData2);
+
+        // Act
+        List<GameData> games = gameDataAccess.getAllGames();
+
+        // Assert
+        assertEquals(2, games.size());
+        // Additional assertions for each game data object in the list
+        // You can compare the properties of each game data object retrieved from the database
+    }
+
+    @Test
+    public void testGetAllGames_Negative() throws Exception {
+        // Negative test for getting all games when there are no games in the database
+        // Act
+        List<GameData> games = gameDataAccess.getAllGames();
+
+        // Assert
+        assertTrue(games.isEmpty());
+    }
+
+    @Test
+    public void testUpdateGame_Positive() throws Exception {
+        // Positive test for updating a game
+        // Arrange
+        int gameID = 1;
+        String newUsername = "newUser";
+        String teamColor = "WHITE";
+
+        // Add a game to the database
+        GameData gameData = new GameData(gameID, "oldUser", "oldUser", "oldGame", new ChessGame());
+        addGameToDatabase(gameData);
+
+        // Act
+        gameDataAccess.updateGame(gameID, newUsername, teamColor);
+
+        // Assert
+        GameData updatedGameData = gameDataAccess.getGame(gameID);
+        assertEquals(newUsername, teamColor.equalsIgnoreCase("WHITE") ? updatedGameData.getWhiteUsername() : updatedGameData.getBlackUsername());
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void testUpdateGame_Negative_InvalidTeamColor() throws Exception {
+        // Negative test for updating a game with invalid team color
+        // Arrange
+        int gameID = 1;
+        String newUsername = "newUser";
+        String teamColor = "INVALID_COLOR";
+
+        // Add a game to the database
+        GameData gameData = new GameData(gameID, "oldUser", "oldUser", "oldGame", new ChessGame());
+        addGameToDatabase(gameData);
+
+        // Act
+        gameDataAccess.updateGame(gameID, newUsername, teamColor);
+
+        // Assert (Exception expected)
     }
 
     private void addGameToDatabase(GameData gameData) throws SQLException {
