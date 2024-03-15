02e2efdb911750fd54bacb5e343fdc06244a717e
Garrett Wride <garrett.wride@gmail.com>
2024-03-15 09:32:15 -0600

    evel() mehtod

--- client/src/main/java/ui/Menu.java
+++ client/src/main/java/ui/Menu.java
@@ -1,59 +1,61 @@
-//package ui;
-//
-////import webSocketMessages.Notification;
-//
-//import static ui.EscapeSequences.*;
-//import java.io.PrintStream;
-//import java.nio.charset.StandardCharsets;
-//import java.util.Scanner;
-//
-//
-//
-//public class Menu {
-//    public Menu() {
-//
-//
-//        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
-//        out.print(EscapeSequences.ERASE_SCREEN);
-//        displayMenu(out);
-//        out.print(EscapeSequences.SET_BG_COLOR_BLACK);
-//        out.print(EscapeSequences.SET_TEXT_COLOR_WHITE);
-//
+package ui;
+
+//import webSocketMessages.Notification;
+
+import static ui.EscapeSequences.*;
+import java.io.PrintStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Scanner;
+
+
+
+public class Menu {
+
+    private static MenuClient client;
+    public Menu(String serverUrl) {
+
+        client = new MenuClient(serverUrl);
+        var out = new PrintStream(System.out, true, StandardCharsets.UTF_8);
+        out.print(EscapeSequences.ERASE_SCREEN);
+        displayMenu(out);
+        out.print(EscapeSequences.SET_BG_COLOR_BLACK);
+        out.print(EscapeSequences.SET_TEXT_COLOR_WHITE);
+
+    }
+
+    public static void main(String[] args){
+        new Menu("qwerty");
+    }
+
+    private void displayMenu(PrintStream out){
+        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
+        System.out.print(client.help());
+
+        Scanner scanner = new Scanner(System.in);
+        var result = "";
+        while (!result.equals("quit")) {
+            printPrompt();
+            String line = scanner.nextLine();
+
+            try {
+                result = client.eval(line);
+                System.out.print(BLUE + result);
+            } catch (Throwable e) {
+                var msg = e.toString();
+                System.out.print(msg);
+            }
+        }
+        System.out.println();
+    }
+
+//    public void notify(Notification notification) {
+//        System.out.println(RED + notification.message());
+//        printPrompt();
 //    }
-//
-//    public static void main(String[] args){
-//        new Menu();
-//    }
-//
-//    private static void displayMenu(PrintStream out){
-//        System.out.println("\uD83D\uDC36 Welcome to the pet store. Sign in to start.");
-//        System.out.print(client.help());
-//
-//        Scanner scanner = new Scanner(System.in);
-//        var result = "";
-//        while (!result.equals("quit")) {
-//            printPrompt();
-//            String line = scanner.nextLine();
-//
-//            try {
-//                result = client.eval(line);
-//                System.out.print(BLUE + result);
-//            } catch (Throwable e) {
-//                var msg = e.toString();
-//                System.out.print(msg);
-//            }
-//        }
-//        System.out.println();
-//    }
-//
-////    public void notify(Notification notification) {
-////        System.out.println(RED + notification.message());
-////        printPrompt();
-////    }
-//
-//    private void printPrompt() {
-//        System.out.print("\n" + RESET + ">>> " + GREEN);
-//    }
-//
-//
-//}
+
+    private void printPrompt() {
+        System.out.print("\n" + RESET + ">>> " + GREEN);
+    }
+
+
+}
