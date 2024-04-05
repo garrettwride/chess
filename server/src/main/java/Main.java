c5e6579a937ef10b4af2efdb6cb77bd2d159dd4e
Garrett Wride <garrett.wride@gmail.com>
2024-04-05 14:50:10 -0600

    phase 6 set up

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -6,6 +6,7 @@ public class Main {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
 
+
         Server server = new Server();
         int port = server.run(8080);
     }
