b4136d68002bae0717f9aa0631653bfacef00fe1
Garrett Wride <garrett.wride@gmail.com>
2024-04-09 15:14:27 -0600

    subclasses

--- shared/src/main/java/webSocketMessages/userCommands/UserGameCommand.java
+++ shared/src/main/java/webSocketMessages/userCommands/UserGameCommand.java
@@ -72,17 +72,4 @@ public static class JoinPlayerCommand extends UserGameCommand {
         return playerColor;
     }
 }
-public class JoinObserverCommand extends UserGameCommand {
-    private final int gameID;
-
-    public JoinObserverCommand(int gameID, String authToken) {
-        super(authToken);
-        this.commandType = CommandType.JOIN_OBSERVER;
-        this.gameID = gameID;
-    }
-
-    public int getGameID() {
-        return gameID;
-    }
-}
 
