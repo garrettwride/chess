27109680762b5e7183882ed762d548f49cdd763c
Garrett Wride <garrett.wride@gmail.com>
2024-04-10 09:23:37 -0600

    ws facade

--- client/src/main/java/websocket/WebSocketFacade.java
+++ client/src/main/java/websocket/WebSocketFacade.java
@@ -1,10 +1,15 @@
 package websocket;
 
+import chess.ChessGame;
+import chess.ChessMove;
 import com.google.gson.Gson;
 import exception.ResponseException;
 import webSocketMessages.*;
 import webSocketMessages.*;
 import webSocketMessages.serverMessages.ServerMessage;
+import webSocketMessages.userCommands.JoinObserverCommand;
+import webSocketMessages.userCommands.JoinPlayerCommand;
+import webSocketMessages.userCommands.MakeMoveCommand;
 import webSocketMessages.userCommands.UserGameCommand;
 
 import javax.websocket.*;
@@ -46,19 +51,28 @@ public class WebSocketFacade extends Endpoint {
     public void onOpen(Session session, EndpointConfig endpointConfig) {
     }
 
-    public void enterPetShop(String visitorName) throws ResponseException {
+    public void joinPlayer(int gameID, ChessGame.TeamColor playerColor, String authToken) throws ResponseException {
         try {
-            var action = new Action(Action.Type.ENTER, visitorName);
-            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+            var command = new JoinPlayerCommand(gameID, playerColor, authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
         } catch (IOException ex) {
             throw new ResponseException(500, ex.getMessage());
         }
     }
 
-    public void leavePetShop(String visitorName) throws ResponseException {
+    public void joinObserver(int gameID, String authToken) throws ResponseException {
         try {
-            var action = new Action(Action.Type.EXIT, visitorName);
-            this.session.getBasicRemote().sendText(new Gson().toJson(action));
+            var command = new JoinObserverCommand(gameID, authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
+        } catch (IOException ex) {
+            throw new ResponseException(500, ex.getMessage());
+        }
+    }
+
+    public void makeMove(int gameID, ChessMove move, String authToken) throws ResponseException {
+        try {
+            var command = new MakeMoveCommand(gameID, move, authToken);
+            this.session.getBasicRemote().sendText(new Gson().toJson(command));
             this.session.close();
         } catch (IOException ex) {
             throw new ResponseException(500, ex.getMessage());
---
+++ shared/src/main/java/webSocketMessages/userCommands/LeaveCommand.java
@@ -0,0 +1,15 @@
+package webSocketMessages.userCommands;
+
+public class LeaveCommand extends UserGameCommand {
+    private final int gameID;
+
+    public LeaveCommand(int gameID, String authToken) {
+        super(authToken);
+        this.commandType = CommandType.LEAVE;
+        this.gameID = gameID;
+    }
+
+    public int getGameID() {
+        return gameID;
+    }
+}
